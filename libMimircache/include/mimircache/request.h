747310099bdceb47bed22fcedaf2f6b449636654
Jason <peter.waynechina@gmail.com>
2020-04-21 14:25:42 -0400

    [misc] update strcpy usage to avoid compilation problem when used for C++ application

--- libMimircache/include/mimircache/request.h
+++ libMimircache/include/mimircache/request.h
@@ -5,7 +5,13 @@
 #ifndef MIMIRCACHE_REQUEST_H
 #define MIMIRCACHE_REQUEST_H
 
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
 #include <glib.h>
+#include <string.h>
 #include "const.h"
 
 
@@ -14,24 +20,20 @@
 /* need to optimize this for CPU cacheline */
 typedef struct request {
 //  gint64 ts;
-  gint64 real_time;
-  /* pointer to obj_id */
+  gint64 real_time;  // vscsi uses millisec timestamp
+  /* pointer to obj_id if obj_id is str otherwise we use this as 64-bit int */
   gpointer obj_id_ptr;
   /* obj_id_type of content can be either size_t(OBJ_ID_NUM) or char*(OBJ_ID_STR)*/
   obj_id_type_t obj_id_type;
 
-  gint32 size;
-  gint64 ttl;
-  // used to check whether current request is a valid request
+  gint32 obj_size;
+  gint32 ttl;
+  // used to indicate whether current request is a valid request and end of trace
+  gint32 op;
   gboolean valid;
 
-  gint64 op;
   gint64 extra_data;
-  void* extra_data_ptr;
-
-  //  char obj_id[MAX_OBJ_ID_LEN];
-//  size_t block_unit_size;
-//  size_t disk_sector_size;
+  void *extra_data_ptr;
 
   // not used
   /* id of cache server, used in akamaiSimulator */
@@ -41,9 +43,9 @@ typedef struct request {
 } request_t;
 
 
-static inline request_t* new_request(obj_id_type_t obj_id_type){
-  request_t* req = g_new0(request_t, 1);
-  req->size = 1;
+static inline request_t *new_request(obj_id_type_t obj_id_type) {
+  request_t *req = g_new0(request_t, 1);
+  req->obj_size = 1;
   req->op = -1;
   req->valid = TRUE;
   req->obj_id_type = obj_id_type;
@@ -58,10 +60,10 @@ static inline request_t* new_request(obj_id_type_t obj_id_type){
 }
 
 
-static inline request_t *clone_request(request_t *req){
-  request_t* req_new = g_new0(request_t, 1);
+static inline request_t *clone_request(request_t *req) {
+  request_t *req_new = g_new0(request_t, 1);
   memcpy(req_new, req, sizeof(request_t));
-  if (req->obj_id_type == OBJ_ID_STR){
+  if (req->obj_id_type == OBJ_ID_STR) {
     req_new->obj_id_ptr = g_new0(char, MAX_OBJ_ID_LEN);
     strcpy(req_new->obj_id_ptr, req->obj_id_ptr);
   }
@@ -69,7 +71,7 @@ static inline request_t *clone_request(request_t *req){
 }
 
 
-static inline void free_request(request_t* req){
+static inline void free_request(request_t *req) {
   if (req->extra_data_ptr)
     g_free(req->extra_data_ptr);
   if (req->obj_id_type == OBJ_ID_STR)
@@ -77,6 +79,8 @@ static inline void free_request(request_t* req){
   g_free(req);
 }
 
-
+#ifdef __cplusplus
+}
+#endif
 
 #endif //MIMIRCACHE_REQUEST_H
