87c5af9ce184b14dc0920fba23fa8afc7abe0e13
Jason <peter.waynechina@gmail.com>
2023-01-08 23:14:31 -0500

    allow slru to have different segment sizes

--- libCacheSim/cache/eviction/SLRU.c
+++ libCacheSim/cache/eviction/SLRU.c
@@ -14,18 +14,28 @@
 extern "C" {
 #endif
 
+#define SLRU_MAX_N_SEG 16
+#define DEBUG_MODE
+#undef DEBUG_MODE
+
 typedef struct SLRU_params {
   cache_obj_t **lru_heads;
   cache_obj_t **lru_tails;
   int64_t *lru_n_bytes;
   int64_t *lru_n_objs;
-  int64_t per_seg_max_size;
+  int64_t *lru_max_n_bytes;
   int n_seg;
 } SLRU_params_t;
 
+static void SLRU_promote_to_next_seg(cache_t *cache, const request_t *req,
+                                     cache_obj_t *obj);
+static void SLRU_cool(cache_t *cache, const request_t *req, const int id);
+bool SLRU_can_insert(cache_t *cache, const request_t *req);
+static void SLRU_parse_params(cache_t *cache,
+                              const char *cache_specific_params);
+
 // ############################## debug functions ##############################
-#define DEBUG_MODE
-#undef DEBUG_MODE
+
 #ifdef DEBUG_MODE
 #define DEBUG_PRINT_CACHE_STATE(cache, params, req)                            \
   do {                                                                         \
@@ -51,123 +61,90 @@ typedef struct SLRU_params {
     printf("\n");                                        \
   } while (0)
 
+static void _SLRU_verify_lru_size(cache_t *cache);
+bool SLRU_get_debug(cache_t *cache, const request_t *req);
+
 #else
 #define DEBUG_PRINT_CACHE_STATE(cache, params, req)
 #define DEBUG_PRINT_CACHE(cache, params)
 #endif
 
-#undef DEBUG_PRINT_CACHE_STATE
-#define DEBUG_PRINT_CACHE_STATE(cache, params, req)
+// ######################## end user facing function ###########################
+cache_t *SLRU_init(const common_cache_params_t ccache_params,
+                   const char *cache_specific_params) {
+  cache_t *cache = cache_struct_init("SLRU", ccache_params);
+  cache->cache_init = SLRU_init;
+  cache->cache_free = SLRU_free;
+  cache->get = SLRU_get;
+  cache->check = SLRU_check;
+  cache->insert = SLRU_insert;
+  cache->evict = SLRU_evict;
+  cache->remove = SLRU_remove;
+  cache->to_evict = SLRU_to_evict;
+  cache->init_params = cache_specific_params;
+  cache->can_insert = SLRU_can_insert;
 
-static void _SLRU_verify_lru_size(cache_t *cache) {
-  SLRU_params_t *params = (SLRU_params_t *)cache->eviction_params;
+  if (ccache_params.consider_obj_metadata) {
+    cache->obj_md_size = 8 * 2;
+  } else {
+    cache->obj_md_size = 0;
+  }
+
+  cache->eviction_params = (SLRU_params_t *)malloc(sizeof(SLRU_params_t));
+  SLRU_params_t *params = (SLRU_params_t *)(cache->eviction_params);
+  memset(params, 0, sizeof(SLRU_params_t));
+  params->n_seg = 4;
+
+  if (cache_specific_params != NULL) {
+    SLRU_parse_params(cache, cache_specific_params);
+  }
+
+  if (params->lru_max_n_bytes == NULL) {
+    // if the user does not specify segment size
+    params->lru_max_n_bytes = calloc(params->n_seg, sizeof(int64_t));
+    for (int i = 0; i < params->n_seg; i++) {
+      params->lru_max_n_bytes[i] =
+          (int64_t)ccache_params.cache_size / params->n_seg;
+    }
+  }
+
+  params->lru_heads =
+      (cache_obj_t **)malloc(sizeof(cache_obj_t *) * params->n_seg);
+  params->lru_tails =
+      (cache_obj_t **)malloc(sizeof(cache_obj_t *) * params->n_seg);
+  params->lru_n_objs = (int64_t *)malloc(sizeof(int64_t) * params->n_seg);
+  params->lru_n_bytes = (int64_t *)malloc(sizeof(int64_t) * params->n_seg);
+
   for (int i = 0; i < params->n_seg; i++) {
-    int64_t n_objs = 0;
-    int64_t n_bytes = 0;
-    cache_obj_t *obj = params->lru_heads[i];
-    while (obj != NULL) {
-      n_objs += 1;
-      n_bytes += obj->obj_size;
-      obj = obj->queue.next;
-    }
-    assert(n_objs == params->lru_n_objs[i]);
-    assert(n_bytes == params->lru_n_bytes[i]);
+    params->lru_heads[i] = NULL;
+    params->lru_tails[i] = NULL;
+    params->lru_n_objs[i] = 0;
+    params->lru_n_bytes[i] = 0;
   }
+
+  // update slru cache name
+  int n = snprintf(cache->cache_name, CACHE_NAME_ARRAY_LEN, "S%dLRU(%d",
+                   params->n_seg,
+                   (int)(params->lru_max_n_bytes[0] * 100 / cache->cache_size));
+  for (int i = 1; i < params->n_seg; i++) {
+    n += snprintf(cache->cache_name + n, CACHE_NAME_ARRAY_LEN - n, ":%d",
+                  (int)(params->lru_max_n_bytes[i] * 100 / cache->cache_size));
+  }
+  snprintf(cache->cache_name + n, CACHE_NAME_ARRAY_LEN - n, ")");
+
+  return cache;
 }
 
-bool SLRU_get_debug(cache_t *cache, const request_t *req) {
-  cache->n_req += 1;
-
+void SLRU_free(cache_t *cache) {
   SLRU_params_t *params = (SLRU_params_t *)(cache->eviction_params);
-  DEBUG_PRINT_CACHE_STATE(cache, params, req);
-
-  bool cache_hit = cache->check(cache, req, true);
-
-  if (cache_hit) {
-    DEBUG_PRINT_CACHE(cache, params);
-    return cache_hit;
-  }
-
-  if (cache->can_insert(cache, req) == false) {
-    DEBUG_PRINT_CACHE(cache, params);
-    return cache_hit;
-  }
-
-  if (!cache_hit) {
-    while (cache->occupied_byte + req->obj_size + cache->obj_md_size >
-           cache->cache_size) {
-      cache->evict(cache, req, NULL);
-    }
-
-    cache->insert(cache, req);
-  }
-
-  DEBUG_PRINT_CACHE(cache, params);
-
-  return cache_hit;
+  free(params->lru_max_n_bytes);
+  free(params->lru_heads);
+  free(params->lru_tails);
+  free(params->lru_n_objs);
+  free(params->lru_n_bytes);
+  cache_struct_free(cache);
 }
 
-// ######################## internal function ###########################
-/* SLRU cannot insert an object larger than segment size */
-bool SLRU_can_insert(cache_t *cache, const request_t *req) {
-  SLRU_params_t *params = (SLRU_params_t *)cache->eviction_params;
-  bool can_insert = cache_can_insert_default(cache, req);
-  return can_insert &&
-         (req->obj_size + cache->obj_md_size <= params->per_seg_max_size);
-}
-
-/**
- * @brief move an object from ith LRU into (i-1)th LRU, cool
- * (i-1)th LRU if it is full, where the n_seg th LRU is the most recent
- *
- * @param cache
- * @param i
- */
-static void SLRU_cool(cache_t *cache, const request_t *req, const int id) {
-  SLRU_params_t *params = (SLRU_params_t *)(cache->eviction_params);
-  DEBUG_PRINT_CACHE_STATE(cache, params, req);
-
-  if (id == 0) return SLRU_evict(cache, req, NULL);
-
-  cache_obj_t *obj = params->lru_tails[id];
-  DEBUG_ASSERT(obj != NULL);
-  DEBUG_ASSERT(obj->SLRU.lru_id == id);
-  remove_obj_from_list(&params->lru_heads[id], &params->lru_tails[id], obj);
-  prepend_obj_to_head(&params->lru_heads[id - 1], &params->lru_tails[id - 1],
-                      obj);
-  obj->SLRU.lru_id = id - 1;
-  params->lru_n_bytes[id] -= obj->obj_size;
-  params->lru_n_bytes[id - 1] += obj->obj_size;
-  params->lru_n_objs[id]--;
-  params->lru_n_objs[id - 1]++;
-
-  // If lower LRUs are full
-  while (params->lru_n_bytes[id - 1] > params->per_seg_max_size) {
-    SLRU_cool(cache, req, id - 1);
-  }
-}
-
-/**
- * @brief promote the object from the current segment to the next (i+1) segment
- */
-static void SLRU_promote_to_next_seg(cache_t *cache, const request_t *req,
-                                     cache_obj_t *obj) {
-  SLRU_params_t *params = (SLRU_params_t *)(cache->eviction_params);
-  DEBUG_PRINT_CACHE_STATE(cache, params, req);
-
-  int id = obj->SLRU.lru_id;
-  remove_obj_from_list(&params->lru_heads[id], &params->lru_tails[id], obj);
-  params->lru_n_bytes[id] -= obj->obj_size + cache->obj_md_size;
-  params->lru_n_objs[id]--;
-
-  obj->SLRU.lru_id += 1;
-  prepend_obj_to_head(&params->lru_heads[id + 1], &params->lru_tails[id + 1],
-                      obj);
-  params->lru_n_bytes[id + 1] += obj->obj_size + cache->obj_md_size;
-  params->lru_n_objs[id + 1]++;
-}
-
-// ######################## external function ###########################
 bool SLRU_get(cache_t *cache, const request_t *req) {
 #ifdef DEBUG_MODE
   return SLRU_get_debug(cache, req);
@@ -200,7 +177,8 @@ bool SLRU_check(cache_t *cache, const request_t *req, const bool update_cache) {
   } else {
     SLRU_promote_to_next_seg(cache, req, obj);
 
-    while (params->lru_n_bytes[obj->SLRU.lru_id] > params->per_seg_max_size) {
+    while (params->lru_n_bytes[obj->SLRU.lru_id] >
+           params->lru_max_n_bytes[obj->SLRU.lru_id]) {
       // if the LRU is full
       SLRU_cool(cache, req, obj->SLRU.lru_id);
     }
@@ -218,7 +196,7 @@ cache_obj_t *SLRU_insert(cache_t *cache, const request_t *req) {
   int nth_seg = -1;
   for (int i = 0; i < params->n_seg; i++) {
     if (params->lru_n_bytes[i] + req->obj_size + cache->obj_md_size <=
-        params->per_seg_max_size) {
+        params->lru_max_n_bytes[i]) {
       nth_seg = i;
       break;
     }
@@ -246,6 +224,7 @@ cache_obj_t *SLRU_insert(cache_t *cache, const request_t *req) {
 
 cache_obj_t *SLRU_to_evict(cache_t *cache) {
   SLRU_params_t *params = (SLRU_params_t *)(cache->eviction_params);
+  DEBUG_PRINT_CACHE_STATE(cache, params, req);
   for (int i = 0; i < params->n_seg; i++) {
     if (params->lru_n_bytes[i] > 0) {
       return params->lru_tails[i];
@@ -256,28 +235,18 @@ cache_obj_t *SLRU_to_evict(cache_t *cache) {
 void SLRU_evict(cache_t *cache, const request_t *req,
                 cache_obj_t *evicted_obj) {
   SLRU_params_t *params = (SLRU_params_t *)(cache->eviction_params);
-  DEBUG_PRINT_CACHE_STATE(cache, params, req);
 
-  int nth_seg = 0;
-  for (int i = 0; i < params->n_seg; i++) {
-    if (params->lru_n_bytes[i] > 0) {
-      nth_seg = i;
-      break;
-    }
-  }
-
-  cache_obj_t *obj = params->lru_tails[nth_seg];
-  DEBUG_ASSERT(obj != NULL);
+  cache_obj_t *obj = SLRU_to_evict(cache);
 
   if (evicted_obj != NULL) {
     memcpy(evicted_obj, obj, sizeof(cache_obj_t));
   }
 
-  params->lru_n_bytes[nth_seg] -= obj->obj_size + cache->obj_md_size;
-  params->lru_n_objs[nth_seg]--;
+  params->lru_n_bytes[obj->SLRU.lru_id] -= obj->obj_size + cache->obj_md_size;
+  params->lru_n_objs[obj->SLRU.lru_id]--;
 
-  remove_obj_from_list(&params->lru_heads[nth_seg], &params->lru_tails[nth_seg],
-                       obj);
+  remove_obj_from_list(&params->lru_heads[obj->SLRU.lru_id],
+                       &params->lru_tails[obj->SLRU.lru_id], obj);
   cache_evict_base(cache, obj, true);
 }
 
@@ -326,7 +295,23 @@ static void SLRU_parse_params(cache_t *cache,
       if (strlen(end) > 2) {
         ERROR("param parsing error, find string \"%s\" after number\n", end);
       }
-
+    } else if (strcasecmp(key, "seg-size") == 0) {
+      int n_seg = 0;
+      int64_t seg_size_sum = 0;
+      int64_t seg_size_array[SLRU_MAX_N_SEG];
+      char *v = strsep((char **)&value, ":");
+      while (v != NULL) {
+        seg_size_array[n_seg++] = (int64_t)strtol(v, &end, 0);
+        seg_size_sum += seg_size_array[n_seg - 1];
+        printf("%s %d\n", v, seg_size_array[n_seg - 1]);
+        v = strsep((char **)&value, ":");
+      }
+      params->n_seg = n_seg;
+      params->lru_max_n_bytes = calloc(params->n_seg, sizeof(int64_t));
+      for (int i = 0; i < n_seg; i++) {
+        params->lru_max_n_bytes[i] = (int64_t)(
+            (double)seg_size_array[i] / seg_size_sum * cache->cache_size);
+      }
     } else if (strcasecmp(key, "print") == 0) {
       printf("current parameters: %s\n", SLRU_current_params(params));
       exit(0);
@@ -338,61 +323,114 @@ static void SLRU_parse_params(cache_t *cache,
   free(old_params_str);
 }
 
-void SLRU_free(cache_t *cache) {
-  SLRU_params_t *params = (SLRU_params_t *)(cache->eviction_params);
-  free(params->lru_heads);
-  free(params->lru_tails);
-  free(params->lru_n_objs);
-  free(params->lru_n_bytes);
-  cache_struct_free(cache);
+// ######################## internal function ###########################
+/* SLRU cannot insert an object larger than segment size */
+bool SLRU_can_insert(cache_t *cache, const request_t *req) {
+  SLRU_params_t *params = (SLRU_params_t *)cache->eviction_params;
+  bool can_insert = cache_can_insert_default(cache, req);
+  return can_insert &&
+         (req->obj_size + cache->obj_md_size <= params->lru_max_n_bytes[0]);
 }
 
-cache_t *SLRU_init(const common_cache_params_t ccache_params,
-                   const char *cache_specific_params) {
-  cache_t *cache = cache_struct_init("SLRU", ccache_params);
-  cache->cache_init = SLRU_init;
-  cache->cache_free = SLRU_free;
-  cache->get = SLRU_get;
-  cache->check = SLRU_check;
-  cache->insert = SLRU_insert;
-  cache->evict = SLRU_evict;
-  cache->remove = SLRU_remove;
-  cache->to_evict = SLRU_to_evict;
-  cache->init_params = cache_specific_params;
-  cache->get_occupied_byte = cache_get_occupied_byte_default;
-  cache->get_n_obj = cache_get_n_obj_default;
-  cache->can_insert = SLRU_can_insert;
-
-  if (ccache_params.consider_obj_metadata) {
-    cache->obj_md_size = 8 * 2;
-  } else {
-    cache->obj_md_size = 0;
-  }
-
-  cache->eviction_params = (SLRU_params_t *)malloc(sizeof(SLRU_params_t));
+/**
+ * @brief move an object from ith LRU into (i-1)th LRU, cool
+ * (i-1)th LRU if it is full, where the n_seg th LRU is the most recent
+ *
+ * @param cache
+ * @param i
+ */
+static void SLRU_cool(cache_t *cache, const request_t *req, const int id) {
   SLRU_params_t *params = (SLRU_params_t *)(cache->eviction_params);
-  params->n_seg = 4;
+  DEBUG_PRINT_CACHE_STATE(cache, params, req);
 
-  if (cache_specific_params != NULL) {
-    SLRU_parse_params(cache, cache_specific_params);
+  if (id == 0) return SLRU_evict(cache, req, NULL);
+
+  cache_obj_t *obj = params->lru_tails[id];
+  DEBUG_ASSERT(obj != NULL);
+  DEBUG_ASSERT(obj->SLRU.lru_id == id);
+  remove_obj_from_list(&params->lru_heads[id], &params->lru_tails[id], obj);
+  prepend_obj_to_head(&params->lru_heads[id - 1], &params->lru_tails[id - 1],
+                      obj);
+  obj->SLRU.lru_id = id - 1;
+  params->lru_n_bytes[id] -= obj->obj_size;
+  params->lru_n_bytes[id - 1] += obj->obj_size;
+  params->lru_n_objs[id]--;
+  params->lru_n_objs[id - 1]++;
+
+  // If lower LRUs are full
+  while (params->lru_n_bytes[id - 1] > params->lru_max_n_bytes[id - 1]) {
+    SLRU_cool(cache, req, id - 1);
   }
+}
 
-  params->per_seg_max_size = ccache_params.cache_size / params->n_seg;
-  params->lru_heads =
-      (cache_obj_t **)malloc(sizeof(cache_obj_t *) * params->n_seg);
-  params->lru_tails =
-      (cache_obj_t **)malloc(sizeof(cache_obj_t *) * params->n_seg);
-  params->lru_n_objs = (int64_t *)malloc(sizeof(int64_t) * params->n_seg);
-  params->lru_n_bytes = (int64_t *)malloc(sizeof(int64_t) * params->n_seg);
+/**
+ * @brief promote the object from the current segment to the next (i+1)
+ * segment
+ */
+static void SLRU_promote_to_next_seg(cache_t *cache, const request_t *req,
+                                     cache_obj_t *obj) {
+  SLRU_params_t *params = (SLRU_params_t *)(cache->eviction_params);
+  DEBUG_PRINT_CACHE_STATE(cache, params, req);
 
+  int id = obj->SLRU.lru_id;
+  remove_obj_from_list(&params->lru_heads[id], &params->lru_tails[id], obj);
+  params->lru_n_bytes[id] -= obj->obj_size + cache->obj_md_size;
+  params->lru_n_objs[id]--;
+
+  obj->SLRU.lru_id += 1;
+  prepend_obj_to_head(&params->lru_heads[id + 1], &params->lru_tails[id + 1],
+                      obj);
+  params->lru_n_bytes[id + 1] += obj->obj_size + cache->obj_md_size;
+  params->lru_n_objs[id + 1]++;
+}
+
+// ############################## debug functions ##############################
+static void _SLRU_verify_lru_size(cache_t *cache) {
+  SLRU_params_t *params = (SLRU_params_t *)cache->eviction_params;
   for (int i = 0; i < params->n_seg; i++) {
-    params->lru_heads[i] = NULL;
-    params->lru_tails[i] = NULL;
-    params->lru_n_objs[i] = 0;
-    params->lru_n_bytes[i] = 0;
+    int64_t n_objs = 0;
+    int64_t n_bytes = 0;
+    cache_obj_t *obj = params->lru_heads[i];
+    while (obj != NULL) {
+      n_objs += 1;
+      n_bytes += obj->obj_size;
+      obj = obj->queue.next;
+    }
+    assert(n_objs == params->lru_n_objs[i]);
+    assert(n_bytes == params->lru_n_bytes[i]);
+  }
+}
+
+bool SLRU_get_debug(cache_t *cache, const request_t *req) {
+  cache->n_req += 1;
+
+  SLRU_params_t *params = (SLRU_params_t *)(cache->eviction_params);
+  DEBUG_PRINT_CACHE_STATE(cache, params, req);
+
+  bool cache_hit = cache->check(cache, req, true);
+
+  if (cache_hit) {
+    DEBUG_PRINT_CACHE(cache, params);
+    return cache_hit;
   }
 
-  return cache;
+  if (cache->can_insert(cache, req) == false) {
+    DEBUG_PRINT_CACHE(cache, params);
+    return cache_hit;
+  }
+
+  if (!cache_hit) {
+    while (cache->occupied_byte + req->obj_size + cache->obj_md_size >
+           cache->cache_size) {
+      cache->evict(cache, req, NULL);
+    }
+
+    cache->insert(cache, req);
+  }
+
+  DEBUG_PRINT_CACHE(cache, params);
+
+  return cache_hit;
 }
 
 #ifdef __cplusplus
