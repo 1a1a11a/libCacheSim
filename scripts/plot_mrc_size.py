4577d7b0a323d2b49de783f7199ed0f9b2897d9c
midsterx <mimosk25@gmail.com>
2023-10-07 01:19:34 -0400

    fix: set default for --ignore-obj-size to False while parsing cli args

--- scripts/plot_mrc_size.py
+++ scripts/plot_mrc_size.py
@@ -199,7 +199,7 @@ if __name__ == "__main__":
                    type=str,
                    default="",
                    help="used by csv trace")
-    p.add_argument("--ignore-obj-size", action="store_true", default=True)
+    p.add_argument("--ignore-obj-size", action="store_true", default=False)
     p.add_argument("--num-thread", type=int, default=-1)
     p.add_argument("--trace-format", type=str, default="oracleGeneral")
     p.add_argument("--name", type=str, default="")
--- scripts/plot_mrc_time.py
+++ scripts/plot_mrc_time.py
@@ -181,7 +181,7 @@ if __name__ == "__main__":
                    type=str,
                    default="",
                    help="used by csv trace")
-    p.add_argument("--ignore-obj-size", action="store_true", default=True)
+    p.add_argument("--ignore-obj-size", action="store_true", default=False)
     p.add_argument("--byte-miss-ratio", action="store_true", default=False)
     p.add_argument("--num-thread", type=int, default=-1)
     p.add_argument("--name", type=str, default="")
