db6bd314c9e16aa096f7cf4096851d79f6ec5fd2
Juncheng Yang <1a1a11a@users.noreply.github.com>
2024-01-01 11:35:00 -0500

    allow the algorithm name to be S3-FIFO (#44)

--- libCacheSim/bin/cachesim/cache_init.h
+++ libCacheSim/bin/cachesim/cache_init.h
@@ -121,7 +121,7 @@ static inline cache_t *create_cache(const char *trace_path,
     cache = Sieve_Belady_init(cc_params, eviction_params);
   } else if (strcasecmp(eviction_algo, "s3lru") == 0) {
     cache = S3LRU_init(cc_params, eviction_params);
-  } else if (strcasecmp(eviction_algo, "s3fifo") == 0) {
+  } else if (strcasecmp(eviction_algo, "s3fifo") == 0 || strcasecmp(eviction_algo, "s3-fifo") == 0) {
     cache = S3FIFO_init(cc_params, eviction_params);
   } else if (strcasecmp(eviction_algo, "s3fifod") == 0) {
     cache = S3FIFOd_init(cc_params, eviction_params);
