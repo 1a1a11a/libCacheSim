e584eadebff59ea36df5578486150d46ee7bba44
haochengxia <xhc_1007@163.com>
2024-06-21 00:36:49 +0800

    Fix scripts for traceAnalyzer

--- scripts/traceAnalysis/run.sh
+++ scripts/traceAnalysis/run.sh
@@ -2,7 +2,8 @@
 
 dataname=$1
 
-python3 access_pattern.py ${dataname}.access
+python3 access_pattern.py ${dataname}.accessRtime
+python3 access_pattern.py ${dataname}.accessVtime
 python3 req_rate.py ${dataname}.reqRate_w300
 python3 size.py ${dataname}.size
 python3 reuse.py ${dataname}.reuse
@@ -11,5 +12,5 @@ python3 popularity.py ${dataname}.popularity
 python3 size_heatmap.py ${dataname}.sizeWindow_w300
 # python3 futureReuse.py ${dataname}.access
 
-python3 popularity_decay.py ${dataname}.popularityDecay_w300
+python3 popularity_decay.py ${dataname}.popularityDecay_w300_obj
 python3 reuse_heatmap.py ${dataname}.reuseWindow_w300
