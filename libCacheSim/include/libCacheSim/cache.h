702f55d4d83d8d3f48b0ea8a82958426eb422e00
Muhammad Haekal M. A. <haekal.muhamat@gmail.com>
2025-03-03 14:41:28 +0700

    Reapply "include admission name and param to the output"
    This reverts commit 850f5f0dfa1468f90a8b2c9542f39a8e762880d1.

--- libCacheSim/include/libCacheSim/cache.h
+++ libCacheSim/include/libCacheSim/cache.h
@@ -70,6 +70,7 @@ typedef void (*cache_print_cache_func_ptr)(const cache_t *);
 #define EVICTION_AGE_ARRAY_SZE 320
 #define EVICTION_AGE_LOG_BASE 1.08
 #define CACHE_NAME_ARRAY_LEN 64
+#define CACHE_STAT_NAME_ARRAY_LEN 640
 #define CACHE_INIT_PARAMS_LEN 256
 typedef struct {
   int64_t n_warmup_req;
@@ -86,7 +87,8 @@ typedef struct {
   int64_t curr_rtime;
   int64_t expired_obj_cnt;
   int64_t expired_bytes;
-  char cache_name[CACHE_NAME_ARRAY_LEN];
+
+  char cache_name[CACHE_STAT_NAME_ARRAY_LEN];
 } cache_stat_t;
 
 struct hashtable;
@@ -366,6 +368,21 @@ bool dump_eviction_age(const cache_t *cache, const char *ofilepath);
 bool dump_cached_obj_age(cache_t *cache, const request_t *req,
                          const char *ofilepath);
 
+static inline void generate_cache_name(cache_t *cache ,char *str_dest) {
+  char admis_param[CACHE_INIT_PARAMS_LEN] = "";
+  if (cache->admissioner->init_params) {
+    strncpy(admis_param, (const char *)cache->admissioner->init_params, CACHE_INIT_PARAMS_LEN);
+  } 
+
+  snprintf(str_dest, CACHE_STAT_NAME_ARRAY_LEN, 
+    "evict:%s[%s] admit:%s[%s]",
+    cache->cache_name, 
+    cache->init_params, 
+    cache->admissioner->admissioner_name, 
+    admis_param
+  );
+}
+                        
 #ifdef __cplusplus
 }
 #endif
