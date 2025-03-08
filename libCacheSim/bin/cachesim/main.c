8a36122f4624565167d7f294e5e776e47d9d9aef
Marthenn <89301265+Marthenn@users.noreply.github.com>
2025-03-09 02:05:28 +0700

    CSV output

--- libCacheSim/bin/cachesim/main.c
+++ libCacheSim/bin/cachesim/main.c
@@ -14,6 +14,8 @@
 #include "internal.h"
 
 int main(int argc, char **argv) {
+  // trace,algo,size,miss_ratio,byte_miss_ratio,ignore_obj_size
+  const char *output_csv = "test.csv";
   struct arguments args;
   parse_cmd(argc, argv, &args);
   if (args.n_cache_size == 0) {
@@ -31,21 +33,21 @@ int main(int argc, char **argv) {
       args.reader, args.caches, args.n_cache_size * args.n_eviction_algo, NULL,
       0, args.warmup_sec, args.n_thread, true, true);
 
-  // output to file
-  char output_str[1024];
-  // ensure file path exists
-  char *output_dir = rindex(args.ofilepath, '/');
-  if (output_dir != NULL) {
-    size_t dir_length = output_dir - args.ofilepath;
-    char dir_path[1024];
-    snprintf(dir_path, dir_length + 1, "%s", args.ofilepath);
-    create_dir(dir_path);
-  }
-  FILE *output_file = fopen(args.ofilepath, "a");
-  if (output_file == NULL) {
-    ERROR("cannot open file %s %s\n", args.ofilepath, strerror(errno));
-    exit(1);
-  }
+  // // output to file
+  // char output_str[1024];
+  // // ensure file path exists
+  // char *output_dir = rindex(args.ofilepath, '/');
+  // if (output_dir != NULL) {
+  //   size_t dir_length = output_dir - args.ofilepath;
+  //   char dir_path[1024];
+  //   // snprintf(dir_path, dir_length + 1, "%s", args.ofilepath);
+  //   create_dir(dir_path);
+  // }
+  // FILE *output_file = fopen(args.ofilepath, "a");
+  // if (output_file == NULL) {
+  //   ERROR("cannot open file %s %s\n", args.ofilepath, strerror(errno));
+  //   exit(1);
+  // }
 
   uint64_t size_unit = 1;
   char *size_unit_str = "";
@@ -64,20 +66,28 @@ int main(int argc, char **argv) {
     }
   }
 
-  printf("\n");
+  // printf("\n");
   for (int i = 0; i < args.n_cache_size * args.n_eviction_algo; i++) {
-    snprintf(output_str, 1024,
-             "%s %s cache size %8ld%s, %lld req, miss ratio %.4lf, byte miss "
-             "ratio %.4lf\n",
-             args.reader->trace_path, result[i].cache_name,
-             (long)(result[i].cache_size / size_unit), size_unit_str,
-             (long long)result[i].n_req,
-             (double)result[i].n_miss / (double)result[i].n_req,
-             (double)result[i].n_miss_byte / (double)result[i].n_req_byte);
-    printf("%s", output_str);
-    fprintf(output_file, "%s", output_str);
+    // snprintf(output_str, 1024,
+    //          "%s %s cache size %8ld%s, %lld req, miss ratio %.4lf, byte miss "
+    //          "ratio %.4lf\n",
+    //          args.reader->trace_path, result[i].cache_name,
+    //          (long)(result[i].cache_size / size_unit), size_unit_str,
+    //          (long long)result[i].n_req,
+    //          (double)result[i].n_miss / (double)result[i].n_req,
+    //          (double)result[i].n_miss_byte / (double)result[i].n_req_byte);
+    // printf("%s", output_str);
+    // fprintf(output_file, "%s", output_str);
+    FILE *file = fopen(output_csv, "a");
+    // trace,algo,size,miss_ratio,byte_miss_ratio,ignore_obj_size
+    fprintf(file, "%s,%s,%ld,%lf,%lf,%d\n", args.reader->trace_path, result[i].cache_name,
+            result[i].cache_size,
+            (double)result[i].n_miss / (double)result[i].n_req,
+            (double)result[i].n_miss_byte / (double)result[i].n_req_byte,
+            args.ignore_obj_size);
+    fclose(file);
   }
-  fclose(output_file);
+  // fclose(output_file);
 
   if (args.n_cache_size * args.n_eviction_algo > 0)
     my_free(sizeof(cache_stat_t) * args.n_cache_size * args.n_eviction_algo, result);
