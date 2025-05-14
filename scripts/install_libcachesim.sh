1d95ea45df0850e4579219a33fdd450438a313bb
Laura Stampf <github@isadora.de>
2025-05-14 13:47:17 +0200

    Fix variable spelling mistake of SORUCE -> SOURCE

--- scripts/install_libcachesim.sh
+++ scripts/install_libcachesim.sh
@@ -1,17 +1,20 @@
 #!/bin/bash 
 set -euo pipefail
 
-
-SORUCE=$(readlink -f ${BASH_SOURCE[0]})
-DIR=$(dirname ${SORUCE})
+SOURCE=$(readlink -f ${BASH_SOURCE[0]})
+DIR=$(dirname $SOURCE})
 
 cd ${DIR}/../;
 mkdir _build || true 2>/dev/null;
 cd _build;
+
+
+
 cmake -DCMAKE_BUILD_TYPE=Debug \
       -DCMAKE_C_FLAGS="-Wall -Wextra -Werror -Wno-unused-variable -Wno-unused-function -Wno-unused-parameter -Wno-unused-but-set-variable -Wpedantic -Wformat=2 -Wformat-security -Wshadow -Wwrite-strings -Wstrict-prototypes -Wold-style-definition -Wredundant-decls -Wnested-externs -Wmissing-include-dirs" \
       -DCMAKE_CXX_FLAGS="-Wall -Wextra -Werror -Wno-unused-variable -Wno-unused-function -Wno-unused-parameter -Wno-unused-but-set-variable -Wpedantic -Wformat=2 -Wformat-security -Wshadow -Wwrite-strings -Wstrict-prototypes -Wold-style-definition -Wredundant-decls -Wnested-externs -Wmissing-include-dirs" \
       ..
+
 make -j;
 cd ${DIR}; 
 
