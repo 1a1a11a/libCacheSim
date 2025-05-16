f7feb916e09c06518f5cf7b65d28d9760d71a3e4
Wang Yunchen <mac-wang@outlook.com>
2025-05-16 23:45:10 +0800

    feat(cache/admission): bloomfilter.c remove GHashTable

--- libCacheSim/cache/admission/bloomfilter.c
+++ libCacheSim/cache/admission/bloomfilter.c
@@ -5,6 +5,8 @@
 #include <glib.h>
 #include <stdbool.h>
 
+#include "../../include/libCacheSim/hashmap.h"
+#include "../../include/libCacheSim/hashmap_defs.in"
 #include "../../include/libCacheSim/admissionAlgo.h"
 
 #ifdef __cplusplus
@@ -12,20 +14,22 @@ extern "C" {
 #endif
 
 typedef struct bloomfilter_admission {
-  GHashTable *seen_times;
+  hashmap_t *seen_times;
 } bf_admission_params_t;
 
 bool bloomfilter_admit(admissioner_t *admissioner, const request_t *req) {
   bf_admission_params_t *bf = admissioner->params;
-  gpointer key = GINT_TO_POINTER(req->obj_id);
-  gpointer n_times =
-      g_hash_table_lookup(bf->seen_times, GSIZE_TO_POINTER(req->obj_id));
+  void *key = (void *)(req->obj_id);
+  void *n_times =
+      hashmap_get(bf->seen_times, (const void *)key, sizeof(obj_id_t));
   if (n_times == NULL) {
-    g_hash_table_insert(bf->seen_times, key, GINT_TO_POINTER(1));
+    hashmap_put(bf->seen_times, (const void *)key, sizeof(obj_id_t),
+                (void *)(1));
     return false;
   } else {
-    g_hash_table_insert(bf->seen_times, key,
-                        GINT_TO_POINTER(GPOINTER_TO_INT(n_times) + 1));
+    // g_hash_table_insert(..., GINT_TO_POINTER(GPOINTER_TO_INT(n_times) + 1))
+    hashmap_put(bf->seen_times, (const void *)key, sizeof(obj_id_t),
+                ((void *) (long) (((int) (long) (n_times)) + 1)));
     return true;
   }
 }
@@ -36,7 +40,8 @@ admissioner_t *clone_bloomfilter_admissioner(admissioner_t *admissioner) {
 
 void free_bloomfilter_admissioner(admissioner_t *admissioner) {
   struct bloomfilter_admission *bf = admissioner->params;
-  g_hash_table_destroy(bf->seen_times);
+  hashmap_destroy(bf->seen_times);
+  free(bf->seen_times);
   free(bf);
   if (admissioner->init_params) {
     free(admissioner->init_params);
@@ -56,7 +61,12 @@ admissioner_t *create_bloomfilter_admissioner(const char *init_params) {
   bf_admission_params_t *bf_params =
       (bf_admission_params_t *)malloc(sizeof(bf_admission_params_t));
   memset(bf_params, 0, sizeof(struct bloomfilter_admission));
-  bf_params->seen_times = g_hash_table_new(g_direct_hash, g_direct_equal);
+  bf_params->seen_times = (hashmap_t *)malloc(sizeof(hashmap_t));
+  hashmap_create_options_t options = {
+      .initial_capacity = 16,
+      .comparer = obj_id_comparer,
+      .hasher = obj_id_hasher};
+  hashmap_create_ex(options, bf_params->seen_times);
 
   admissioner->params = bf_params;
   admissioner->clone = clone_bloomfilter_admissioner;
