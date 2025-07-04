02e893132b2a937f727f3b9771182e30cc668f14
Percy <xhc_1007@163.com>
2025-07-04 10:41:01 -0400

    Skip if dep is installed

--- scripts/install_dependency.sh
+++ scripts/install_dependency.sh
@@ -194,8 +194,20 @@ install_lightgbm() {
 # Install Zstd
 install_zstd() {
 	log_info "Installing Zstd..."
+        local zstd_version="1.5.0"
+        # Check if Zstd exists
+        if command -v zstd &>/dev/null; then
+                local installed_version
+                installed_version=$(zstd --version | grep -oP 'v\K[0-9.]+')
+                if [[ $installed_version == "$zstd_version" ]]; then
+                        log_info "Zstd version $zstd_version already installed."
+                        return 0
+                else
+                        log_info "Zstd is installed but version is $installed_version (expecting $zstd_version). Proceeding to install..."
+                fi
+        fi
+	# Install
 	pushd /tmp/ >/dev/null
-	local zstd_version="1.5.0"
 	if [[ ! -f "zstd-${zstd_version}.tar.gz" ]]; then
 		wget "https://github.com/facebook/zstd/releases/download/v${zstd_version}/zstd-${zstd_version}.tar.gz"
 		tar xf "zstd-${zstd_version}.tar.gz"
