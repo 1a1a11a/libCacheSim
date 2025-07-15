c5f536a6d3ab82764dd9edf7a6c9aa75ef7881b4
Akhil Tulluri <akhilesh@tulluri.me>
2025-07-14 23:31:42 -0400

    sphinx rtd theme + some docs

--- docs/conf.py
+++ docs/conf.py
@@ -16,12 +16,12 @@ author = 'Juncheng Yang'
 extensions = []
 
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'Doxygen*']
 
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 html_static_path = ['_static']
